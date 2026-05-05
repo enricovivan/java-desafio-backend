43c838821f8e563bea512dab153751025d7bdf6c
Enrico Vivan <enricovivan21@gmail.com>
2026-05-04 22:41:28 -0300

    first upload

---
+++ src/main/java/dev/enricovivan/java_rocketseat_desafio_junior/JavaRocketseatDesafioJuniorApplication.java
@@ -0,0 +1,13 @@
+package dev.enricovivan.java_rocketseat_desafio_junior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRocketseatDesafioJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRocketseatDesafioJuniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/enricovivan/java_rocketseat_desafio_junior/JavaRocketseatDesafioJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.enricovivan.java_rocketseat_desafio_junior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRocketseatDesafioJuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
