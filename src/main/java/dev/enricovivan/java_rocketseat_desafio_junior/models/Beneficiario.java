2c095590e6859ac1ee70543d307803672a00bac0
Enrico Vivan <enricovivan21@gmail.com>
2026-05-04 22:50:03 -0300

    feat: adicionado o model do beneficiario

---
+++ src/main/java/dev/enricovivan/java_rocketseat_desafio_junior/models/Beneficiario.java
@@ -0,0 +1,100 @@
+package dev.enricovivan.java_rocketseat_desafio_junior.models;
+
+import java.util.Date;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Beneficiarios")
+public class Beneficiario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    @Column(nullable = false)
+    private Date dataNascimento;
+
+    @Column(nullable = false)
+    private Date dataInclusao;
+
+    private Date dataAtualizacao;
+
+    public Beneficiario() {
+
+    }
+
+    public Beneficiario(UUID id, String nome, String telefone, Date dataNascimento, Date dataInclusao, Date dataAtualizacao) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+        this.dataInclusao = dataInclusao;
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Date getDataInclusao() {
+        return dataInclusao;
+    }
+
+    public void setDataInclusao(Date dataInclusao) {
+        this.dataInclusao = dataInclusao;
+    }
+
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    
+
+    
+
+}
