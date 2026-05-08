73f36f01c7b8a957055d6cb584b82084a576e796
Enrico Vivan <enricovivan21@gmail.com>
2026-05-08 18:01:08 -0300

    feat: adicionadas 3 funções para lidar com adição de documentos em um beneficiario ao mesmo tempo

--- src/main/java/dev/enricovivan/java_rocketseat_desafio_junior/services/BeneficiarioService.java
+++ src/main/java/dev/enricovivan/java_rocketseat_desafio_junior/services/BeneficiarioService.java
@@ -25,17 +25,68 @@ public class BeneficiarioService {
         this.documentoRepository = documentoRepository;
     }
 
-    // cadastrar beneficiario
+    // cadastrar beneficiario simples ✅
     public Beneficiario cadastrarBeneficiario(Beneficiario beneficiario) {
         return this.beneficiarioRepository.save(beneficiario);
     }
 
-    // listar todos os beneficiarios cadastrados
+    // cadastra beneficiario com documentos (manualmente)
+    @Transactional
+    public Beneficiario cadastrarBeneficiarioComDocumentosManuelamente(Beneficiario beneficiario){
+
+        // pega os documentos que vieram na requisição (se houverem)
+        List<Documento> documentosBeneficiario = beneficiario.getDocumentos();
+
+        // limpa os documentos da requisicao
+        beneficiario.setDocumentos(null);
+
+        // primerio cadastra o beneficiario
+        Beneficiario beneficiarioCadastrado = beneficiarioRepository.save(beneficiario);
+
+        // se há documentos, salvamos
+        if (documentosBeneficiario != null && !documentosBeneficiario.isEmpty()) {
+
+            documentosBeneficiario.stream()
+                .forEach(doc -> {
+                    doc.setBeneficiario(beneficiarioCadastrado);
+                });
+
+            List<Documento> documentosBeneficiarioSalvos = documentoRepository.saveAll(documentosBeneficiario);
+
+            // volta a referencia para o beneficiario para que possa retornar na requisição
+            beneficiarioCadastrado.setDocumentos(documentosBeneficiarioSalvos);
+        }
+
+        return beneficiarioCadastrado;
+
+    }
+
+    // cadastra o beneficiario e os documetnos automaticamente (usando o cascade.all do model)
+    @Transactional
+    public Beneficiario cadastrarBeneficiarioComDocumentosAutomaticamente(Beneficiario beneficiario){
+
+        // verifica se veio com documentos
+        if (beneficiario.getDocumentos() != null && !beneficiario.getDocumentos().isEmpty()) {
+            // definimos quem é o pai do documento:
+            beneficiario.getDocumentos().stream()
+                .forEach(doc -> {
+                    doc.setBeneficiario(beneficiario);
+                });
+        }
+
+        // salva o beneficiario c documentos
+        // como usamos CascadeType.ALL na entidade, salvar o beneficiário 
+        // salva automaticamente todos os documentos associados a ele
+        return beneficiarioRepository.save(beneficiario);
+
+    }
+
+    // listar todos os beneficiarios cadastrados ✅
     public List<Beneficiario> listarBeneficiarios() {
         return this.beneficiarioRepository.findAll();
     }
 
-    // lista os documetos do beneficiario
+    // lista os documetos do beneficiario ✅
     @Transactional(readOnly=true)
     public List<Documento> getDocumentosDoBeneficiario(UUID uuidBeneficiario) {
 
@@ -47,7 +98,7 @@ public class BeneficiarioService {
         return documentoRepository.findByBeneficiarioId(uuidBeneficiario);
     }
 
-    // atualiza beneficiario
+    // atualiza beneficiario ✅
     public Beneficiario updateBeneficiario(UUID uuiBeneficiario, Beneficiario req){
         return beneficiarioRepository.findById(uuiBeneficiario)
             .map(beneficiario -> {
@@ -60,7 +111,7 @@ public class BeneficiarioService {
             .orElseThrow(() -> new EntityNotFoundException("Beneficiário não encontrado"));
     }
 
-    // remove um baneficiario
+    // remove um baneficiario ✅
     public Beneficiario deleteBeneficiario(UUID uuid){
         
         // como delete by id retorna void, e  precisamos retornar o beneficiario nessa função
